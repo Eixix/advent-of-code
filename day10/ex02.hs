6292c25883ecaa8c09593530c0a751e7afda6515
Eixix <tobiasbetz@hotmail.de>
2021-12-10 23:15:35 +0100

    Day 10

--- day10/ex02.hs
+++ day10/ex02.hs
@@ -6,37 +6,44 @@ import System.IO
     openFile,
   )
 
+quicksort :: Ord a => [a] -> [a]
+quicksort [] = []
+quicksort (p : xs) = lesser ++ p : greater
+  where
+    lesser = quicksort $ filter (< p) xs
+    greater = quicksort $ filter (>= p) xs
+
 initBrackets :: Num a => [Char] -> a
 initBrackets [] = error "Obacht"
 initBrackets (x : xs) = brackets [x] xs 0
 
 brackets :: Num a => [Char] -> [Char] -> a -> a
-brackets _ [] a = a
-brackets [] (y : ys) a
-  | y == '(' = brackets [y] ys a
-  | y == '[' = brackets [y] ys a
-  | y == '{' = brackets [y] ys a
-  | y == '<' = brackets [y] ys a
-  | y == ')' = brackets [] ys (a + 3)
-  | y == ']' = brackets [] ys (a + 57)
-  | y == '}' = brackets [] ys (a + 1197)
-  | y == '>' = brackets [] ys (a + 25137)
+brackets [] [] a = a
+brackets (xs) [] a
+  | last xs == '(' = brackets (init xs) [] a * 5 + 1
+  | last xs == '[' = brackets (init xs) [] a * 5 + 2
+  | last xs == '{' = brackets (init xs) [] a * 5 + 3
+  | last xs == '<' = brackets (init xs) [] a * 5 + 4
+brackets [] (y : ys) a = brackets [y] ys a
 brackets (x : xs) (y : ys) a
   | y == '(' = brackets (y : x : xs) ys a
   | y == '[' = brackets (y : x : xs) ys a
   | y == '{' = brackets (y : x : xs) ys a
   | y == '<' = brackets (y : x : xs) ys a
-  | y == ')' && x /= '(' = brackets (x : xs) ys (a + 3)
-  | y == ']' && x /= '[' = brackets (x : xs) ys (a + 57)
-  | y == '}' && x /= '{' = brackets (x : xs) ys (a + 1197)
-  | y == '>' && x /= '<' = brackets (x : xs) ys (a + 25137)
+  | y == ')' && x /= '(' = 0
+  | y == ']' && x /= '[' = 0
+  | y == '}' && x /= '{' = 0
+  | y == '>' && x /= '<' = 0
   | otherwise = brackets xs ys a
 
 main :: IO ()
 main = do
   let list = []
-  handle <- openFile "simple.csv" ReadMode
+  handle <- openFile "input.csv" ReadMode
   contents <- hGetContents handle
   let singlelines = lines contents
-  print $ sum $ map initBrackets singlelines
+      resultSorted = quicksort $ filter (/= 0) (map initBrackets singlelines)
+  print resultSorted
+  print $ length resultSorted
+  print $ resultSorted !! (length resultSorted `div` 2)
   hClose handle
