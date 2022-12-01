e53c93bb57e050bb79b694a9af810c4a4c6a4b3b
Tobias <t.betz@cortex-media.de>
2022-12-01 08:31:41 +0100

    2022

---
+++ 2022/01/01.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	first()
+	second()
+}
+
+func first() {
+	file, _ := os.Open("challenge.txt")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	highestSum := 0.0
+	sum := 0.0
+
+	for scanner.Scan() {
+		if (scanner.Text() == "") {
+			highestSum = math.Max(highestSum, sum)
+			sum = 0.0
+		} else {
+			tmp, _ := strconv.ParseFloat(scanner.Text(), 32)
+			sum += tmp
+		}
+	}
+
+	fmt.Println(highestSum)
+}
+
+func second() {
+	file, _ := os.Open("challenge.txt")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	
+	var sums []float64
+	sum := 0.0
+
+	for scanner.Scan() {
+		if (scanner.Text() == "") {
+			sums = append(sums, sum)
+			sum = 0.0
+		} else {
+			tmp, _ := strconv.ParseFloat(scanner.Text(), 32)
+			sum += tmp
+		}
+	}
+
+	sort.Slice(sums, func(i, j int) bool {
+    	return sums[i] > sums[j]
+	})
+	var toReturn = 0.0
+
+	for i := 0; i < 3; i++ {
+		toReturn += sums[i]
+	}
+
+	fmt.Println(toReturn)
+}
