6eb9c7bd091b448e097de3709acd44f672da5c27
Eixix <tobiasbetz@hotmail.de>
2022-12-02 10:54:00 +0100

    2 Learn to read again

--- 2022/02/02.go
+++ 2022/02/02.go
@@ -100,11 +100,11 @@ func C(input string) int {
 func A2(input string) int {
 	switch input {
 	case "X":
-		return 2 + 6
+		return 3 + 0
 	case "Y":
 		return 1 + 3
 	case "Z":
-		return 3 + 0
+		return 2 + 6
 	default:
 		panic("Can't happen")
 	}
@@ -113,11 +113,11 @@ func A2(input string) int {
 func B2(input string) int {
 	switch input {
 	case "X":
-		return 3 + 6
+		return 1 + 0
 	case "Y":
 		return 2 + 3
 	case "Z":
-		return 1 + 0
+		return 3 + 6
 	default:
 		panic("Can't happen")
 	}
@@ -126,11 +126,11 @@ func B2(input string) int {
 func C2(input string) int {
 	switch input {
 	case "X":
-		return 1 + 6
+		return 2 + 0
 	case "Y":
 		return 3 + 3
 	case "Z":
-		return 2 + 0
+		return 1 + 6
 	default:
 		panic("Can't happen")
 	}
