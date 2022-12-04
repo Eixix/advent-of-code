b2eacf5e38dd34007749fb4840ff3e52023f7cd9
Eixix <tobiasbetz@hotmail.de>
2022-12-04 19:48:01 +0100

    Finished 4

--- 2022/04/04.go
+++ 2022/04/04.go
@@ -4,12 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 
 func main() {
 	first()
+	second()
 }
 
 func first() {
@@ -24,18 +26,45 @@ func first() {
 		text := scanner.Text()
 		s := strings.Split(text, ",")
 
-		firstPartLower := strings.Split(s[0], "-")[0]
-		firstPartHigher := strings.Split(s[0], "-")[1]
-		secondPartLower := strings.Split(s[1], "-")[0]
-		secondPartHigher := strings.Split(s[1], "-")[1]
+		firstPartLower, _ := strconv.Atoi(strings.Split(s[0], "-")[0])
+		firstPartHigher, _ := strconv.Atoi(strings.Split(s[0], "-")[1])
+		secondPartLower, _ := strconv.Atoi(strings.Split(s[1], "-")[0])
+		secondPartHigher, _ := strconv.Atoi(strings.Split(s[1], "-")[1])
 
-		if firstPartLower >= secondPartLower && firstPartHigher <= secondPartHigher {
+		firstInSecond := firstPartLower >= secondPartLower && firstPartHigher <= secondPartHigher
+		secondInFirst := secondPartLower >= firstPartLower && secondPartHigher <= firstPartHigher
+
+		if firstInSecond || secondInFirst {
 			toReturn++
-			fmt.Println(firstPartLower, firstPartHigher, secondPartLower, secondPartHigher)
-		} else if secondPartLower >= firstPartLower && secondPartHigher <= firstPartHigher {
+		} 
+	}
+
+	fmt.Println(toReturn)
+
+}
+
+func second() {
+	file, _ := os.Open("challenge.txt")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	toReturn := 0
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		s := strings.Split(text, ",")
+
+		firstPartLower, _ := strconv.Atoi(strings.Split(s[0], "-")[0])
+		firstPartHigher, _ := strconv.Atoi(strings.Split(s[0], "-")[1])
+		secondPartLower, _ := strconv.Atoi(strings.Split(s[1], "-")[0])
+		secondPartHigher, _ := strconv.Atoi(strings.Split(s[1], "-")[1])
+
+		overlapping := firstPartLower <= secondPartHigher && firstPartLower >= secondPartHigher || firstPartHigher <= secondPartHigher && firstPartHigher >= secondPartLower || secondPartLower <= firstPartHigher && secondPartLower >= firstPartHigher || secondPartHigher <= firstPartHigher && secondPartHigher >= firstPartLower
+
+		if overlapping {
 			toReturn++
-			fmt.Println(firstPartLower, firstPartHigher, secondPartLower, secondPartHigher)
-		}
+		} 
 	}
 
 	fmt.Println(toReturn)
