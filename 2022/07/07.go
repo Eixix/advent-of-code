680c47dcddd40c98ab3220d821754bde889bf29b
Tobias <t.betz@cortex-media.de>
2022-12-07 11:45:05 +0100

    WIP 7.2

--- 2022/07/07.go
+++ 2022/07/07.go
@@ -8,19 +8,15 @@ import (
 	"strings"
 )
 
-func main() {
-	first()
-}
-
 type FileNode struct {
 	name        string
 	parent      *FileNode
 	isDirectory bool
-	size        int
+	size        int64
 	children    map[string]*FileNode
 }
 
-func first() {
+func main() {
 	file, _ := os.Open("challenge.txt")
 	defer file.Close()
 
@@ -33,7 +29,7 @@ func first() {
 		text := scanner.Text()
 		words := strings.Fields(text)
 		if words[0] == "$" {
-			if words[1] == "cd" {
+			if words[1] == "cd" && words[2] != "/" {
 				if words[2] == ".." {
 					directory = directory.parent
 				} else {
@@ -46,18 +42,23 @@ func first() {
 		} else if words[0] == "dir" {
 			directory.children[words[1]] = &FileNode{words[1], directory, true, 0, make(map[string]*FileNode)}
 		} else {
-			size, _ := strconv.Atoi(words[0])
+			size, _ := strconv.ParseInt(words[0], 10, 64)
 			directory.children[words[1]] = &FileNode{words[1], directory, false, size, nil}
 		}
 	}
 
-	toReturn, size := addSizesToDirs(rootDirectory)
+	// full Tree
+	rootDirectory, _ = addSizesToDirs(rootDirectory)
 
-	fmt.Println(toReturn.children["a"].size, size)
+	fmt.Println(addSmallerThan100K(rootDirectory))
+
+	freeSpace := 70000000 - rootDirectory.size
+	fmt.Println(findSmallestFolderToDelete(rootDirectory, freeSpace))
 }
 
-func addSizesToDirs(rootNode FileNode) (FileNode, int) {
-	parentSize := 0
+func addSizesToDirs(rootNode FileNode) (FileNode, int64) {
+	var parentSize int64
+	parentSize = 0
 	for _, node := range rootNode.children {
 		if node.isDirectory {
 			_, node.size = addSizesToDirs(*node)
@@ -66,4 +67,39 @@ func addSizesToDirs(rootNode FileNode) (FileNode, int) {
 	}
 	rootNode.size = parentSize
 	return rootNode, parentSize
+}
+
+func addSmallerThan100K(rootNode FileNode) int64 {
+	var toReturn int64
+	toReturn = 0
+	for _, node := range rootNode.children {
+		if node.isDirectory && node.size <= 100000 {
+			toReturn += node.size + addSmallerThan100K(*node)
+		} else if node.isDirectory {
+			toReturn += addSmallerThan100K(*node)
+		}
+	}
+	return toReturn
+}
+
+func findSmallestFolderToDelete(rootNode FileNode, freeSpace int64) FileNode {
+	var targetFreeSpace int64
+	targetFreeSpace = 30000000
+	deltaFreeSpace := targetFreeSpace - freeSpace
+
+	bestDirectory := rootNode
+
+	for _, node := range rootNode.children {
+		if node.isDirectory {
+			childBestDirectory := findSmallestFolderToDelete(*node, freeSpace)
+
+			if deltaFreeSpace-childBestDirectory.size < deltaFreeSpace-node.size {
+				bestDirectory = childBestDirectory
+			} else {
+				bestDirectory = *node
+			}
+		}
+	}
+
+	return bestDirectory
 }
